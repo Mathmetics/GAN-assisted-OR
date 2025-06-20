926c8e86112cefd5b10076992238bc2d597c873c
Zihaobit <65904995+Zihaobit@users.noreply.github.com>
2025-06-21 00:45:55 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+cd ~/Downloads/多端同步/GAN-Assisted-OR 
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/Mathmetics/GAN-assisted-OR.git
+git branch -M 
+git push -u origin main
