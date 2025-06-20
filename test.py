ca2bc8493c932bbb04e61de1ca3e33c4a2f59ae5
Zihaobit <65904995+Zihaobit@users.noreply.github.com>
2025-06-21 01:33:22 +0800

    Fix bug in test.py

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-cd ~/Downloads/多端同步/GAN-Assisted-OR 
-git init
-git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/Mathmetics/GAN-assisted-OR.git
-git branch -M 
-git push -u origin main
-
-new one 
+1
